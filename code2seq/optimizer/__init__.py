2b46ef35893ff4b57bb4357d7611ee50bb8136bb
Dmitry Pasechnyuk <pasechnyuk2004@gmail.com>
2021-03-08 13:48:22 +0800

    1-report-state

--- code2seq/optimizer/__init__.py
+++ code2seq/optimizer/__init__.py
@@ -1,6 +1,8 @@
 from .nadam import Nadam
+from .svrg import SVRG
 
 __all__ = [
     "Nadam"
+    "SVRG"
 ]
 
--- code2seq/utils/training.py
+++ code2seq/utils/training.py
@@ -4,7 +4,8 @@ import numpy
 import torch
 from omegaconf import DictConfig
 from torch.optim import Adam, Optimizer, SGD, Adadelta, Adagrad, Adamax, RMSprop, LBFGS
-from optimizer import Nadam
+import torch_optimizer as optim
+from optimizer import Nadam, SVRG
 from torch.optim.lr_scheduler import _LRScheduler, LambdaLR
 
 
@@ -43,6 +44,64 @@ def configure_optimizers_alon(
     
     elif hyper_parameters.optimizer == "RMSprop":
         optimizer = RMSprop(parameters, hyper_parameters.learning_rate, weight_decay=hyper_parameters.weight_decay)
+    
+    elif hyper_parameters.optimizer == "AdaBound":
+        optimizer = optim.AdaBound(parameters, lr=hyper_parameters.learning_rate, weight_decay=hyper_parameters.weight_decay, 
+                                   betas= (0.9, 0.999),
+                                   final_lr = 0.1,
+                                   gamma=1e-3,
+                                   eps= 1e-8,
+                                   amsbound=False)
+        
+    elif hyper_parameters.optimizer == "Yogi":
+        optimizer = optim.Yogi(parameters, lr=hyper_parameters.learning_rate, weight_decay=hyper_parameters.weight_decay, 
+                                   betas=(0.9, 0.999),
+                                    eps=1e-3,
+                                    initial_accumulator=1e-6)
+        
+    elif hyper_parameters.optimizer == "Apollo":
+        optimizer = optim.Apollo(parameters, lr=hyper_parameters.learning_rate, weight_decay=hyper_parameters.weight_decay, 
+            beta=0.9,
+            eps=1e-4,
+            warmup=0,
+            init_lr=0.01
+        )
+        
+    elif hyper_parameters.optimizer == "A2GradExp":
+        optimizer = optim.A2GradExp(parameters, lr=hyper_parameters.learning_rate,
+    beta=10.0,
+    lips=10.0,
+    rho=0.5
+)
+        
+    elif hyper_parameters.optimizer == "DiffGrad":
+        optimizer = optim.DiffGrad(parameters, lr=hyper_parameters.learning_rate, weight_decay=hyper_parameters.weight_decay, 
+    betas=(0.9, 0.999),
+    eps=1e-8
+)
+        
+    elif hyper_parameters.optimizer == "Lamb":
+        optimizer = optim.Lamb(parameters, lr=hyper_parameters.learning_rate, weight_decay=hyper_parameters.weight_decay,
+    betas=(0.9, 0.999),
+    eps=1e-8
+)
+        
+    elif hyper_parameters.optimizer == "RAdam":
+        optimizer = optim.RAdam(parameters, lr=hyper_parameters.learning_rate, weight_decay=hyper_parameters.weight_decay,
+    betas=(0.9, 0.999),
+    eps=1e-8
+)
+        
+    elif hyper_parameters.optimizer == "NovoGrad":
+        optimizer = optim.NovoGrad(parameters, lr=hyper_parameters.learning_rate, weight_decay=hyper_parameters.weight_decay,
+    betas=(0.9, 0.999),
+    eps=1e-8,
+    grad_averaging=False,
+    amsgrad=False
+)
+    
+    elif hyper_parameters.optimizer == "SVRG":
+        optimizer = SVRG(parameters, hyper_parameters.learning_rate, freq=80)
     
     elif hyper_parameters.optimizer == "LBFGS":
         optimizer = LBFGS(parameters, hyper_parameters.learning_rate)
