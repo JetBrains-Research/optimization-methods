e34b8f1d17deb21e8a67affe091ecf3804c4f94f
Anton <antoshaigorevic@gmail.com>
2021-08-13 13:15:00 +0300

    chrfpp precision recall f1

--- textmetric/metrics.py
+++ textmetric/metrics.py
@@ -7,7 +7,7 @@ from nltk.translate.meteor_score import single_meteor_score
 from nltk.translate.bleu_score import corpus_bleu, sentence_bleu
 from nltk.translate.chrf_score import corpus_chrf, sentence_chrf
 from rouge import Rouge
-import chrFpp
+from chrFpp import computeChrF
 from bert_score import score as bert_score
 
 from time import perf_counter
@@ -108,6 +108,20 @@ class Metrics:
                 'chrF': corpus_chrf([[ref] for ref in self.refs], self.hyps)
             }
         }
+
+    def chrfpp(self):
+
+        total_f1, _, _, _, sentence_scores = computeChrF(self.refs, self.hyps)
+
+        return {
+            'scores': {
+                'chrf++': np.array(sentence_scores)
+            },
+            'score': {
+                'chrf++': total_f1
+            }
+
+        }
     
     # def chrFplusplus(self):
     #     raise NotImplementedError()
@@ -173,3 +187,67 @@ class Metrics:
             'scores': scores_dict,
             'score': score_dict
         }
+
+    def prec_rec_f1(self):
+        true_positive = false_positive = false_negative = 0
+        prec_scores, rec_scores, f1_scores = [], [], []
+        for hyp, ref in zip(self.hyps, self.refs):
+            tp = fp = fn = 0
+            gt_seq = ref.split()
+            pred_seq = hyp.split()
+
+            if len(gt_seq) == len(pred_seq) and all([g == p for g, p in zip(gt_seq, pred_seq)]):
+                tp += len(gt_seq)
+            else:
+                for pred_subtoken in pred_seq:
+                    if pred_subtoken in gt_seq:
+                        tp += 1
+                    else:
+                        fp += 1
+                for gt_subtoken in gt_seq:
+                    if gt_subtoken not in pred_seq:
+                        fn += 1
+
+            prec_sent, rec_sent, f1_sent = calculate_one_token_metrics(tp, fp, fn)
+
+            prec_scores.append(prec_sent)
+            rec_scores.append(rec_sent)
+            f1_scores.append(f1_sent)
+
+            true_positive += tp
+            false_positive += fp
+            false_negative += fn
+
+        prec_total, rec_total, f1_total = calculate_one_token_metrics(true_positive, false_positive, false_negative)
+
+        return {
+            'scores': {
+                'precision': np.array(prec_scores),
+                'recall': np.array(rec_scores),
+                'f1': np.array(f1_scores)
+            },
+            'score': {
+                'precision': prec_total,
+                'recall': rec_total,
+                'f1': f1_total
+            }
+        }
+
+
+def calculate_one_token_metrics(true_positive: int, false_positive: int, false_negative: int):
+    precision, recall, f1 = 0.0, 0.0, 0.0
+    if true_positive + false_positive > 0:
+        precision = true_positive / (true_positive + false_positive)
+    if true_positive + false_negative > 0:
+        recall = true_positive / (true_positive + false_negative)
+    if precision + recall > 0:
+        f1 = 2 * precision * recall / (precision + recall)
+    return precision, recall, f1
+
+
+if __name__ == "__main__":
+    refs = ['мама мыла раму', 'рыба дышит под водой']
+    hyps = ['мама мыла раму', 'рыба продается в магазине']
+
+    metrics = Metrics(hyps, refs)
+    print(metrics.get_statistics())
