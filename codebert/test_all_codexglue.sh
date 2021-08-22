ec397e64648b15423af7e41967b797e08919e430
dmivilensky <pasechnyuk2004@gmail.com>
2021-08-22 16:49:40 +0000

    table

--- codebert/test_all_codexglue.sh
+++ codebert/test_all_codexglue.sh
@@ -1,8 +1,8 @@
-python3 test.py ./models/CodeBERTa-docstrings-new/SGD/checkpoint_2455.pickle
-python3 test.py ./models/CodeBERTa-docstrings-new/LaSGD/checkpoint_2455.pickle
-python3 test.py ./models/CodeBERTa-docstrings-new/Adam/checkpoint_2455.pickle
-python3 test.py ./models/CodeBERTa-docstrings-new/LaAdam/checkpoint_2455.pickle
-python3 test.py ./models/CodeBERTa-docstrings-new/RAdam/checkpoint_2455.pickle
-python3 test.py ./models/CodeBERTa-docstrings-new/LaRAdam/checkpoint_2455.pickle
-python3 test.py ./models/CodeBERTa-docstrings-new/Lamb/checkpoint_2455.pickle
-python3 test.py ./models/CodeBERTa-docstrings-new/LaLamb/checkpoint_2455.pickle
+python3 test.py ./models/CodeBERTa-docstrings-new/SGD/checkpoint_4910.pickle
+python3 test.py ./models/CodeBERTa-docstrings-new/LaSGD/checkpoint_4910.pickle
+python3 test.py ./models/CodeBERTa-docstrings-new/Adam/checkpoint_4910.pickle
+python3 test.py ./models/CodeBERTa-docstrings-new/LaAdam/checkpoint_4910.pickle
+python3 test.py ./models/CodeBERTa-docstrings-new/RAdam/checkpoint_4910.pickle
+python3 test.py ./models/CodeBERTa-docstrings-new/LaRAdam/checkpoint_4910.pickle
+python3 test.py ./models/CodeBERTa-docstrings-new/Lamb/checkpoint_4910.pickle
+python3 test.py ./models/CodeBERTa-docstrings-new/LaLamb/checkpoint_4910.pickle
