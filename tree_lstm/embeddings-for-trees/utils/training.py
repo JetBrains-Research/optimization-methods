c24dbaa33020614d8ac45857a9ecadffc15b9a94
Anton <antoshaigorevic@gmail.com>
2021-09-18 16:45:55 +0300

    small fix

--- tree_lstm/embeddings-for-trees/utils/training.py
+++ tree_lstm/embeddings-for-trees/utils/training.py
@@ -7,6 +7,7 @@ from optimizer import SVRG, SdLBFGS, BB, RLamb, LaRAdamLamb, KFACOptimizer
 import torch_optimizer as optim
 from torch.optim.lr_scheduler import _LRScheduler, LambdaLR, ReduceLROnPlateau
 from scheduler import MyCyclicLR
+import numpy as np
 from math import sqrt
 
 
@@ -264,3 +265,70 @@ def configure_optimizers_alon(
     else:
         raise ValueError('No such learning rate strategy')
     return [optimizer], [scheduler]
+
+
+def segment_sizes_to_slices(sizes: torch.Tensor) -> List:
+    cum_sums = torch.cumsum(sizes, dim=0)
+    slices = [slice(0, cum_sums[0])]
+    slices += [slice(start, end) for start, end in zip(cum_sums[:-1], cum_sums[1:])]
+    return slices
+
+
+def cut_encoded_data(
+    encoded_data: torch.Tensor, samples_per_label: torch.LongTensor, mask_value: float = -1e9
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """Cut encoded data into batches
+    :param encoded_data: [n contexts; units]
+    :param samples_per_label: [batch size]
+    :param mask_value:
+    :return: [batch size; max context len; units], [batch size; max context len]
+    """
+    batch_size = len(samples_per_label)
+    max_context_len = max(samples_per_label)
+    batched_contexts = encoded_data.new_zeros((batch_size, max_context_len, encoded_data.shape[-1]))
+    attention_mask = encoded_data.new_zeros((batch_size, max_context_len))
+    context_slices = segment_sizes_to_slices(samples_per_label)
+    for i, (cur_slice, cur_size) in enumerate(zip(context_slices, samples_per_label)):
+        batched_contexts[i, :cur_size] = encoded_data[cur_slice]
+        attention_mask[i, cur_size:] = mask_value
+    return batched_contexts, attention_mask
+
+
+def init_weights_normal(m):
+    """Takes in a module and initializes all linear layers with weight
+       values taken from a normal distribution."""
+    classname = m.__class__.__name__
+    # for every Linear layer in a model
+    if classname == 'Linear':
+        y = m.in_features
+        # m.weight.data shoud be taken from a normal distribution
+        m.weight.data.normal_(0.0, 1 / np.sqrt(y))
+        # m.bias.data should be 0
+        if m.bias is not None:
+            m.bias.data.fill_(0)
+    elif classname == 'LSTM':
+        num_layers = m.num_layers
+        sigma = 1 / np.sqrt(m.hidden_size)
+        for i in range(num_layers):
+            getattr(m, 'weight_ih_l' + str(i)).data.normal_(0.0, sigma)
+            getattr(m, 'weight_hh_l' + str(i)).data.normal_(0.0, sigma)
+            if m.bias:
+                getattr(m, 'bias_ih_l' + str(i)).data.fill_(0)
+                getattr(m, 'bias_hh_l' + str(i)).data.fill_(0)
+
+
+def init_weights_const(m, value=0):
+    if classname == 'Linear':
+        m.weight.data.fill_(value)
+        if m.bias is not None:
+            m.bias.data.fill_(value)
+    elif classname == 'LSTM':
+        num_layers = m.num_layers
+        for i in range(num_layers):
+            getattr(m, 'weight_ih_l' + str(i)).data.fill_(value)
+            getattr(m, 'weight_hh_l' + str(i)).data.fill_(value)
+            if m.bias:
+                getattr(m, 'bias_ih_l' + str(i)).data.fill_(value)
+                getattr(m, 'bias_hh_l' + str(i)).data.fill_(value)
+    elif classname == 'Embedding':
+        m.weight.data.fill_(value)
